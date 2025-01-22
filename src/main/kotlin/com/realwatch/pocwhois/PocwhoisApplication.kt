8ec7d8ae191c0a66e4a42bbd2092d74b73e11a36
Dayhana Ramirez <dayhanaramirez45@gmail.com>
2025-01-22 18:15:06 -0500

    Initial project structure

---
+++ src/main/kotlin/com/realwatch/pocwhois/PocwhoisApplication.kt
@@ -0,0 +1,11 @@
+package com.realwatch.pocwhois
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocwhoisApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocwhoisApplication>(*args)
+}
---
+++ src/test/kotlin/com/realwatch/pocwhois/PocwhoisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.realwatch.pocwhois
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocwhoisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
